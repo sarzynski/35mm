904558e565f4ccc1709b948f52847ff80ba560c2
sarzynski <kamil.s.sarzynski@gmail.com>
2018-11-16 19:19:23 +0100

    views update

---
+++ db/migrate/20181116181351_drop_user_id_from_photos.rb
@@ -0,0 +1,5 @@
+class DropUserIdFromPhotos < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :photos, :user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_180116) do
+ActiveRecord::Schema.define(version: 2018_11_16_181351) do
 
   create_table "photos", force: :cascade do |t|
     t.text "description"
