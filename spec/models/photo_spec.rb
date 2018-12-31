b3aaecc198af89e15ea001cc7ab955500f6f7e4b
sarzynski <kamil.s.sarzynski@gmail.com>
2018-12-31 02:45:03 +0100

    update rspec

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,23 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Photo, type: :model do
-  subject { described_class.new }
-
-  it "is valid with valid attributes" do
-    subject.camera = "asfasfasf"
-    subject.film = "asfasfasf"
-    expect(subject).to be_valid
-  end
+  subject { described_class.new}
 
   it "is not valid with too long camera attribute" do
+    subject.film = "aggea"
     subject.camera = "a"*201
-    subject.film = "asfasfasf"
     expect(subject).to_not be_valid
   end
 
   it "is not valid with too long film attribute" do
-    subject.camera = "asfasf"
     subject.film = "a"*201
+    subject.camera = "asrg"
     expect(subject).to_not be_valid
   end
 end
